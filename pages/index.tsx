7e584e9364d0f5d5f9d369fc7327c870f908a756
Gabriel Klein <gabriel.klein@philips.com>
2022-11-07 18:19:43 -0300

    criação do header da página HomeNoAuth

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import Head from "next/head";
-import styles from '../styles/HomeNotAuth.module.scss';
+import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
+import styles from "../styles/homeNoAuth.module.scss" 
 
 const HomeNotAuth = function () {
   return (
-		<>
-			<Head>
-        <title>Home</title>
+    <>
+      <Head>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+				<meta property="og:title" content="Onebitflix" key="title" />
+				<meta name="description" content="Tenha acesso aos melhores conteúdos sobre programação de uma forma simples e fácil." />
       </Head>
-			<main></main>
-		</>
+    <main>
+      <HeaderNoAuth />
+    </main>
+    </>
   );
 };
 
