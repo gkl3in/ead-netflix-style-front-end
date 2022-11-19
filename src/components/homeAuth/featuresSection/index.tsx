4f890856dc1462572aa2f709f52322e1e3952b89
Gabriel Klein <gabriel.klein@philips.com>
2022-11-19 15:27:43 -0300

    Adicionando responsividade e background-image

--- src/components/homeAuth/featuresSection/index.tsx
+++ src/components/homeAuth/featuresSection/index.tsx
@@ -17,9 +17,15 @@ const FeaturedSection = function () {
     <>
       {
         data.data?.map((course: CourseType) => (
-          <div key={course.id} >
+          <div key={course.id}
+            style={{
+              backgroundImage: `linear-gradient(to bottom, #6666661a, #151515), url(${process.env.NEXT_PUBLIC_BASEURL}/${course.thumbnailUrl})`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+              height: "480px",
+            }} >
             <HeaderAuth />
-            <Container>
+            <Container className="pt-4">
               <p className={styles.title}>{course.name}</p>
               <p className={styles.description}>{course.synopsis}</p>
               <Link href={`/courses/${course.id}`} style={{ textDecoration: 'none' }}>
