5e88fd0e5e798841e190470c7bca44ac986a4b98
Gabriel Klein <gabriel.klein@philips.com>
2022-12-13 19:51:25 -0300

    Criação dos elementos da pagina de episodios e da responsividade

--- pages/course/episode/[id].tsx
+++ pages/course/episode/[id].tsx
@@ -5,7 +5,7 @@ import HeaderGeneric from "../../../src/components/common/headerGeneric";
 import courseService, { CourseType } from "../../../src/services/courseService";
 import { useState, useEffect } from "react";
 import PageSpinner from "../../../src/components/common/spinner";
-import { Container } from "reactstrap";
+import { Button, Container } from "reactstrap";
 import ReactPlayer from "react-player";
 
 const EpisodePlayer = function () {
@@ -21,6 +21,14 @@ const EpisodePlayer = function () {
     if (res.status === 200) {
       setCourse(res.data);
     }
+  };
+
+  const handleLastEpisode = () => {
+    router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}`);
+  };
+
+  const handleNextEpisode = () => {
+    router.push(`/course/episode/${episodeOrder - 1}?courseid=${course?.id}`);
   };
 
   useEffect(() => {
@@ -50,6 +58,19 @@ const EpisodePlayer = function () {
               controls
             />
           )}
+          <div className={styles.episodeButtonDiv}>
+            <Button
+              className={styles.episodeButton}
+              disabled={episodeOrder === 0 ? true : false}
+              onClick={handleLastEpisode}><img src="/episode/iconArrowLeft.svg" alt="setaEsquerda" className={styles.arrowImg} /></Button>
+            <Button
+              className={styles.episodeButton}
+              disabled={episodeOrder + 1 === course.episodes.length ? true : false}
+              onClick={handleNextEpisode}><img src="/episode/iconArrowRight.svg" alt="setaDireita" className={styles.arrowImg} /></Button>
+          </div>
+          <p className="text-center pb-4">
+            {course.episodes[episodeOrder].synopsis}
+          </p>
         </Container>
       </main>
     </>
